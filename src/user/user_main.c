19d5754b015e2f80136aa48f80e75664a054c4ef
Simon Arlott <sa.me.uk>
2022-07-17 11:13:23 +0100

    Build with PlatformIO

--- src/user/user_main.c
+++ src/user/user_main.c
@@ -11,13 +11,47 @@
  * You should have received a copy of the GNU General Public License along
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include <user_interface.h>
 #include "driver/swim.h"
 #include "driver/uart.h"
 #include "serial.h"
 
+uint32 ICACHE_FLASH_ATTR
+user_rf_cal_sector_set(void)
+{
+    enum flash_size_map size_map = system_get_flash_size_map();
+    uint32 rf_cal_sec = 0;
+
+    switch (size_map) {
+        case FLASH_SIZE_4M_MAP_256_256:
+            rf_cal_sec = 128 - 8;
+            break;
+
+        case FLASH_SIZE_8M_MAP_512_512:
+            rf_cal_sec = 256 - 5;
+            break;
+
+        case FLASH_SIZE_16M_MAP_512_512:
+        case FLASH_SIZE_16M_MAP_1024_1024:
+            rf_cal_sec = 512 - 5;
+            break;
+
+        case FLASH_SIZE_32M_MAP_512_512:
+        case FLASH_SIZE_32M_MAP_1024_1024:
+            rf_cal_sec = 1024 - 5;
+            break;
+
+        default:
+            rf_cal_sec = 0;
+            break;
+    }
+
+    return rf_cal_sec;
+}
+
 void user_init(void) {
   swim_init();
   uart_init(BIT_RATE_115200, BIT_RATE_115200);
-  uart0_sendStr("ESP-STLINK\r\n");
+  uart0_sendStr("\r\nESP-STLINK\r\n");
   serial_init();
 }
